f79f0bb9919eda43c192fcce0d1e0ca5e5d424b9
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-01-22 12:02:55 +0700

    modified : change component navbar.

--- src/components/common/NavItem.tsx
+++ src/components/common/NavItem.tsx
@@ -6,14 +6,25 @@ export default function NavItem() {
   return (
     <>
       {NavLinks?.map((nav, index) => (
-        <Link
-          key={index}
-          to={nav.href}
-          className="transition-all hover:bg-primary py-1.5 px-2.5 hover:text-primary-foreground hover:rounded-md duration-500 flex items-center gap-2"
-        >
-          {nav.icon}
-          {nav.label}
-        </Link>
+        nav.href.startsWith('http') ? (
+            <a
+              key={index}
+              href={nav.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition-all hover:bg-primary py-1.5 px-2.5 hover:text-primary-foreground hover:rounded-md duration-500 flex items-center gap-2"
+            >
+              {nav.label}
+            </a>
+          ) : (
+            <Link
+              key={index}
+              to={nav.href}
+              className="transition-all hover:bg-primary py-1.5 px-2.5 hover:text-primary-foreground hover:rounded-md duration-500 flex items-center gap-2"
+            >
+              {nav.label}
+            </Link>
+          )
       ))}
       <div className="md:block hidden">
       <ModeToggle  />
